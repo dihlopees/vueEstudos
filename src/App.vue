96f3f46f3540474469cca70b04e8a280992849e5
Ingrid Rieser Lopes de Oliveira <iroliveira@viptechti.int>
2022-07-08 18:25:52 -0300

    pirmeiro comit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,114 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+
+    <div class="input" >
+      <input v-model="campo" placeholder="Digite seu nome aqui:" /> {{aviso}}
+      
+      <button @click="pushLista">Adicionar</button>
+            
+    
+        <h3> Nomes da Mesa:</h3>
+        <div v-for="item in items" :key="item.items">
+        
+        <p> {{item}}</p>
+        
+        </div>
+      
+    </div>
+
+    <div v-if="showRender">
+      <renderizar :text="text" />
+    </div>
+
+    <div class="botao">
+      <button @click="mostrarRender">{{ nomeBotao }}</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import renderizar from "./components/renderizar_compo.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { renderizar },
+  data() {
+    return {
+      title: "Meu Root App",
+      text: "Informação do meu Componente",
+      showRender: false,
+      nomeBotao: "Mostrar Render",
+      items: ["Ingrid" ," Idair", " Paulo"],
+      campo:"",
+      aviso:"",
+      timer:null,
+      
+     
+    }
+  },
+  methods: {
+    mostrarRender() {
+      this.showRender = !this.showRender;
+
+      if (this.showRender == true) {
+        this.nomeBotao = "Esconder Render";
+      } else {
+        this.nomeBotao = "Mostrar Render";
+      }
+    },
+    pushLista() {
+
+      if(this.campo.length > 3){
+            this.items.push(this.campo)
+            alert("Nome adicionado com sucesso")
+      }else {
+        alert("Informe um nome com mais de 3 letras")
+      }
+      
+      
+      this.campo = ""
+      
+      
+      
+    },
+    setarTime(){
+    this.aviso = "Aguardando..";
+    }
+  },
+  watch:{
+    campo(){
+      this.aviso = "Digitando..."
+
+      
+      if(this.timer != null){
+          clearTimeout(this.timer);
+      }
+
+      this.timer = setTimeout(this.setarTime, 1000)
+
+    }
   }
-}
+};
 </script>
 
-<style>
+<style scope>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
+}
+
+.input {
+ border: solid 4px #ccc;
+  margin: 50px;
+  padding: 15px;
+}
+
+button {
+  height: 40px;
+  border-radius: 30px;
+  margin: 50px;
+  background-color: #ccc;
 }
 </style>
---
+++ src/components/renderizar_compo.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="backdroop"  >
+    <div class="modal" :class="{dark}"  >
+      <h1>{{ title }}</h1>
+      <p>{{ text }}</p>
+
+      
+
+      <div class="iconeRedondos" :class="{dark}">
+        <button @click="comprar">
+          <img src="../assets/imagens/batom.png" />
+        </button>
+        <button>
+          <img src="../assets/imagens/boca.png" />
+        </button>
+        <button>
+          <img src="../assets/imagens/flor.png" />
+        </button>
+      </div>
+
+      <div>
+        <button @click="mudarTheme">Mood Dark</button>
+      </div>
+
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text"],
+  data() {
+    return {
+      title: "aqui dentro fica meu componente",
+      dark:false,
+    
+    }
+  },
+  methods:{
+    mudarTheme(){
+      this.dark = !this.dark
+    }
+  }
+};
+</script>
+
+<style scope>
+.modal {
+  background-color: bisque;
+  color: rgb(41, 37, 37);
+  
+}
+
+.modal h1 {
+  margin: 20px;
+  padding-top: 50px;
+}
+.iconeRedondos button {
+  width: 50px;
+  margin: 10px;
+  border-radius: 100px;
+  background-color: crimson;
+}
+
+
+.dark{
+  background-color: black;
+  color: white;
+
+}
+
+.dark button {
+  background-color: #ccc;
+}
+</style>
