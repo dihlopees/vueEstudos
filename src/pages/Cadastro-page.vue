289f094ce6d5b2d6691dafd31ee62c8c9b1861bf
Ingrid Rieser Lopes de Oliveira <iroliveira@viptechti.int>
2022-07-12 00:28:12 -0300

    add imagens em svg, iniciei tela cadastro produto do zero

--- src/pages/Cadastro-page.vue
+++ src/pages/Cadastro-page.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
+    <div class="cadastroProdutos">
+        <form action="" method="post">
+            <label for="POST-name">Nome do Produto:</label>
+            <input id="POST-name" type="text" name="name">
+            <input type="submit" value="Save">
+          </form>
         
     </div>
 </template>
